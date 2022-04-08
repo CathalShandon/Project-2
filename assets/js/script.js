d1eb7b73ea0c9ca20fd82352ddbe2ed6e7bdf3bb
CathalShandon <Cathalshandon@gmail.com>
2022-04-08 20:11:16 +0000

    Add more questions to quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,83 +1,83 @@
-// array of Questions and Answers.
-
+//Question bank
 let questionBank = [
-{
-    question : 'What is the captial of France?',
-    option : ['Luxemberg', 'Paris', 'Vienna', 'Glasglow'],
-    answer : 'Paris'
-},
-{
-    question : 'What is the captial of Croatia?',
-    option : ['Lyon', 'Berlin', 'Zagreb', 'Moscow'],
-    answer : 'Zagreb'
-},
-{
-    question : 'What is the captial of Austria?',
-    option : ['Vienna', 'Salzburg', 'Turin', 'Riga'],
-    answer : 'Vienna'
-},
-{
-    question : 'What is the captial of Moldova?',
-    option : ['Balti', 'Sofia', 'Szeged', 'Chisinau'],
-    aswer : 'Chisinau'
-},
-{
-    question : 'What is the captial of Ukraine?',
-    option : ['Odesa', 'Angers', 'Kiev', 'Hamburg'],
-    answer : 'Kiev'
-},
-{
-    question : 'What is the captial of Greece?',
-    option : ['Sarajevo', 'Zadara', 'Krakow', 'Athens'],
-    answer : 'Athens'
-},
-{
-    question : 'What is the captial of Bulgaria?',
-    option : ['Plovdiv', 'Sofia', 'Bari', 'Burgos'],
-    answer : 'Sofia'
-},
-{
-    question : 'What is the captial of Portugal?',
-    option : ['Porto', 'Lisbon', 'Vigo', 'Zaragoza'],
-    answer : 'Lisbon'
-},
-{
-    question : 'What is the captial of Switzerland?',
-    option : ['Zurich', 'Hamburg', 'Bremen', 'Bern'],
-    answer : 'Bern'
-},
-{
-    question : 'What is the captial of Belgium?',
-    option : ['Ghent', 'Brussels', 'Reims', 'Dresden'],
-    answer : 'Brussels'
-},
-{
-    question : 'What is the captial of Scotland?',
-    option : ['Athens', 'Glasglow', 'Brest', 'Edinburgh'],
-    answer : 'Edinburgh'
-},
-{
-    question : 'What is the captial of Slovenia?',
-    option : ['Bratislava', 'Ljubljana', 'Maribor', 'Graz'],
-    answer : 'Ljubljana'
-},
-{
-    question : 'What is the captial of Slovakia?',
-    option : ['Eger', 'Bratislava', 'Linz', 'Split'],
-    answer : 'Bratislava'
-},
-{
-    question : 'What is the captial of Bosnia and Herzegovina?',
-    option : ['Sarajeve', 'Tuzla', 'Debrecen', 'Presov'],
-    answer : 'Sarajevo'
-},
-{
-    question : 'What is the captial of North Macedonia?',
-    option : ['Bitola', 'Durres', 'Tirana', 'Skopje'],
-    answer : 'Skopje'
-},
+    {
+        question : 'What is the captial of France?',
+        option : ['Luxemberg', 'Paris', 'Vienna', 'Glasglow'],
+        answer : 'Paris'
+    },
+    {
+        question : 'What is the captial of Croatia?',
+        option : ['Lyon', 'Berlin', 'Zagreb', 'Moscow'],
+        rightAnswer : 'Zagreb'
+    },
+    {
+        question : 'What is the captial of Austria?',
+        option : ['Vienna', 'Salzburg', 'Turin', 'Riga'],
+        answer : 'Vienna'
+    },
+    {
+        question : 'What is the captial of Moldova?',
+        option : ['Balti', 'Sofia', 'Szeged', 'Chisinau'],
+        aswer : 'Chisinau'
+    },
+    {
+        question : 'What is the captial of Ukraine?',
+        option : ['Odesa', 'Angers', 'Kiev', 'Hamburg'],
+        answer : 'Kiev'
+    },
+    {
+        question : 'What is the captial of Greece?',
+        option : ['Sarajevo', 'Zadara', 'Krakow', 'Athens'],
+        answer : 'Athens'
+    },
+    {
+        question : 'What is the captial of Bulgaria?',
+        option : ['Plovdiv', 'Sofia', 'Bari', 'Burgos'],
+        answer : 'Sofia'
+    },
+    {
+        question : 'What is the captial of Portugal?',
+        option : ['Porto', 'Lisbon', 'Vigo', 'Zaragoza'],
+        answer : 'Lisbon'
+    },
+    {
+        question : 'What is the captial of Switzerland?',
+        option : ['Zurich', 'Hamburg', 'Bremen', 'Bern'],
+        answer : 'Bern'
+    },
+    {
+        question : 'What is the captial of Belgium?',
+        option : ['Ghent', 'Brussels', 'Reims', 'Dresden'],
+        answer : 'Brussels'
+    },
+    {
+        question : 'What is the captial of Scotland?',
+        option : ['Athens', 'Glasglow', 'Brest', 'Edinburgh'],
+        answer : 'Edinburgh'
+    },
+    {
+        question : 'What is the captial of Slovenia?',
+        option : ['Bratislava', 'Ljubljana', 'Maribor', 'Graz'],
+        answer : 'Ljubljana'
+    },
+    {
+        question : 'What is the captial of Slovakia?',
+        option : ['Eger', 'Bratislava', 'Linz', 'Split'],
+        answer : 'Bratislava'
+    },
+    {
+        question : 'What is the captial of Bosnia and Herzegovina?',
+        option : ['Sarajeve', 'Tuzla', 'Debrecen', 'Presov'],
+        answer : 'Sarajevo'
+    },
+    {
+        question : 'What is the captial of North Macedonia?',
+        option : ['Bitola', 'Durres', 'Tirana', 'Skopje'],
+        answer : 'Skopje'
+    },
+    
+    ];
 
-];
 
 var question = document.getElementById('question');
 var quizContainer = document.getElementById('quiz-container');
@@ -87,14 +87,14 @@ var option1 = document.getElementById('option1');
 var option2 = document.getElementById('option2');
 var option3 = document.getElementById('option3');
 var next = document.querySelector('.next');
-var points = document.getElementById('answer');
+var points = document.getElementById('score');
 var span = document.querySelectorAll('span');
 var i = 0;
 var score = 0;
 
 //Function to display questions
 function displayQuestion() {
-    for (var a=0; a<span.length;a++){
+    for (var a=0;a<span.length;a++){
         span[a].style.background='none';
     }
     question.innerHTML= 'Q.'+(i+1)+' '+questionBank[i].question;
@@ -104,6 +104,7 @@ function displayQuestion() {
     option3.innerHTML= questionBank[i].option[3];
     questionCount.innerHTML= "Question"+' '+(i+1)+' '+'of'+' '+questionBank.length;
 }
+
 //Function to calculate scores
 function calcScore(e){
     if(e.innerHTML===questionBank[i].answer && score<questionBank.length){
@@ -117,7 +118,7 @@ function calcScore(e){
 
 //Function to display next question
 function nextQuestion(){
-    if(i<questionBank.length-0)
+    if(i<questionBank.length-1)
     {
         i=i+1;
         displayQuestion();
@@ -127,6 +128,7 @@ function nextQuestion(){
         scoreboard.style.display= 'block'
     }
 }
+
 //Click events to next button
 next.addEventListener('click',nextQuestion);
 
@@ -149,18 +151,37 @@ function checkAnswer(){
         answers.appendChild(list);
     }
 }
-// increament score for correct answers
 
-function increamentScore(correctAnswerCount) {
-    document.getElementById("right-answer").innerText = correctAnswerCount;
-    score= score+1;
+
+displayQuestion();
+
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
+ function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
+
 }
 
-// increament score for wrong answers
-function increamentWrongAnswer(wrongAnswerCount) {
-    document.getElementById("wrong-answer").innerText = wrongAnswerCount;
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
+function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
+    
+}
+function checkAnswer() {
+
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    runGame(calculatedAnswer[1]);
 
 }
 
-/* final score box */
-
+/* array of Questions and Answers.*/
